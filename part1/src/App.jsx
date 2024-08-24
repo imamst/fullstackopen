398d2f8eb3dd7fa4b697d555330eafdc834465f0
Imam Setiawan <imam.setiawan@javan.co.id>
2024-08-24 12:48:21 +0700

    Exercises 1.3.

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,17 +1,23 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content content={[[part1, exercises1], [part2, exercises2], [part3, exercises3]]} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content content={[part1, part2, part3]} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
@@ -22,7 +28,7 @@ const Header = (props) => {
 
 const Content = (props) => {
   return <>
-    {props.content.map((content, index) => <Part key={index} text={`${content[0]} ${content[1]}`} />)}
+    {props.content.map((content, index) => <Part key={index} text={`${content.name} ${content.exercises}`} />)}
   </>
 }
 
