6c376981fbd170a9367aba7162c6c1d222f5dd75
Imam Setiawan <imam.setiawan@javan.co.id>
2024-08-25 12:47:40 +0700

    Exercises 1.9: unicafe step 4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,13 +4,21 @@ const Statistics = ({good, neutral, bad}) => {
   return (
     <>
       <p><b>statistics</b></p>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      
+      {
+        (good || neutral || bad) ?
+        (
+          <>
+            <p>good {good}</p>
+            <p>neutral {neutral}</p>
+            <p>bad {bad}</p>
 
-      <p>all {good + neutral + bad}</p>
-      <p>average {(good - bad) / (good + neutral + bad)}</p>
-      <p>positive {(good / (good + neutral + bad) * 100)} %</p>
+            <p>all {good + neutral + bad}</p>
+            <p>average {(good - bad) / (good + neutral + bad)}</p>
+            <p>positive {(good / (good + neutral + bad) * 100)} %</p>
+          </>
+        ) : <p>No feedback given</p>
+      }
     </>
   )
 }
