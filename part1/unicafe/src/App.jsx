323beab888d44a4e77fb66d561f134720ddcc240
Imam Setiawan <imam.setiawan@javan.co.id>
2024-08-25 12:35:50 +0700

    Exercises 1.6: unicafe step 1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,47 +1,24 @@
+import { useState } from 'react'
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content content={course.parts} />
-      <Total parts={course.parts} />
+      <p><b>give feedback</b></p>
+      <button onClick={() => setGood(prev => prev + 1)}>good</button>
+      <button onClick={() => setNeutral(prev => prev + 1)}>neutral</button>
+      <button onClick={() => setBad(prev => prev + 1)}>bad</button>
+
+      <p><b>statistics</b></p>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
-}
-
-const Header = (props) => {
-  return <h1>{props.course}</h1>
-}
-
-const Content = (props) => {
-  return <>
-    {props.content.map((content, index) => <Part key={index} text={`${content.name} ${content.exercises}`} />)}
-  </>
-}
-
-const Total = (props) => {
-  return <p>Number of exercises {props.parts?.reduce((acc, part) => part.exercises + acc, 0)}</p>
-}
-
-const Part = (props) => {
-  return <p>{props.text}</p>
 }
 
 export default App
