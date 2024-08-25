8cab2fe818e22980b4f086e63265d38c16827e14
Imam Setiawan <imam.setiawan@javan.co.id>
2024-08-25 16:08:06 +0700

    2.9*: The Phonebook Step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567'}
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newId, setNewId] = useState(persons?.length)
 
   const handleChangeName = (e) => {
     setNewName(e.target.value)
@@ -18,6 +22,7 @@ const App = () => {
   const addPerson = (e) => {
     e.preventDefault()
 
+    // unique name validation
     const isNameAlreadyExists = Boolean(persons.find((person) => person.name == newName)?.name)
 
     if (isNameAlreadyExists) {
@@ -26,22 +31,46 @@ const App = () => {
       return
     }
 
-    setPersons([
+    // entry new data if pass validation
+    const newPersons = [
       ...persons,
       {
+        id: newId + 1,
         name: newName,
         number: newNumber,
       }
-    ])
+    ]
 
+    setPersons(newPersons)
+    setFilteredPersons(newPersons)
+
+    // clear form and set new id
+    setNewId(prev => prev + 1)
     setNewName('')
     setNewNumber('')
+  }
+
+  const [filteredPersons, setFilteredPersons] = useState(persons)
+  const searchPerson = (e) => {
+    setFilteredPersons(
+      persons.filter(
+        person => person.name.includes(e.target.value)
+      )
+    )
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <div>
+        filter shown with: <input onChange={searchPerson} />
+      </div>
+
+      <br />
+
       <form onSubmit={addPerson}>
+        <h1><b>add a new</b></h1>
         <div>
           name: <input value={newName} onChange={handleChangeName} />
         </div>
@@ -54,7 +83,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map((person) => <p key={person.name}>{person.name} {person.number}</p>)}
+        {filteredPersons.map((person) => <p key={person.id}>{person.name} {person.number}</p>)}
       </div>
     </div>
   )
