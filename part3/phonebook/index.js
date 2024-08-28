0b78ab440dd9172fee5d6db594cb411ad41847ae
Imam Setiawan <imam.setiawan@javan.co.id>
2024-08-28 14:59:34 +0700

    3.2: Phonebook backend step 2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -28,6 +28,22 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  const options = {
+    weekday: 'long',       // Day of the week (e.g., Wednesday)
+    year: 'numeric',       // Year (e.g., 2024)
+    month: 'long',         // Month (e.g., August)
+    day: 'numeric',        // Day of the month (e.g., 28)
+    hour: 'numeric',       // Hour (e.g., 11)
+    minute: 'numeric',     // Minute (e.g., 30)
+    second: 'numeric',     // Second (e.g., 15)
+    timeZoneName: 'long'   // Full time zone name (e.g., Greenwich Mean Time)
+  };
+  const time = new Date().toLocaleString('en-US', options);
+  
+  response.send(`Phonebook has info for ${persons.length} people <br/> ${time}`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
