c7b5f344c105af85dd2da0d1980bfe6b4f79c78a
Imam Setiawan <imam.setiawan@javan.co.id>
2024-08-28 16:44:05 +0700

    3.8*: Phonebook backend step 8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
+morgan.token('payload', function (req, res) { return JSON.stringify(req.body) })
+
 app.use(express.json());
-app.use(morgan('tiny'));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :payload'));
 
 let persons = [
   { 
