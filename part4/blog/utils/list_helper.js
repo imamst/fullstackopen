3f1a8d548c95b877dc68ac1129c990478d8b2cf0
Imam Setiawan <imam.setiawan@javan.co.id>
2024-09-10 16:27:53 +0700

    4.4: Helper Functions and Unit Tests, step 2

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((accumulateValue, currentValue) => accumulateValue + currentValue?.likes, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
