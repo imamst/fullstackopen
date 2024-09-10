7dfcdf14bca5defc4a96abf56d7a8e9b3fbde3bf
Imam Setiawan <imam.setiawan@javan.co.id>
2024-09-10 16:39:54 +0700

    4.5*: Helper Functions and Unit Tests, step 3

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -6,7 +6,23 @@ const totalLikes = (blogs) => {
   return blogs.reduce((accumulateValue, currentValue) => accumulateValue + currentValue?.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  let topLikedArticle = {
+    id: null,
+    likes: 0
+  }
+
+  blogs.forEach(element => {
+    if (element.likes > topLikedArticle.likes) {
+      topLikedArticle = element
+    }
+  });
+
+  return topLikedArticle
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
