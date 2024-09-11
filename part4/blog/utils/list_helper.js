2e1b9d6cefad29dd3159fed6c6a081ca046c9974
Imam Setiawan <imam.setiawan@javan.co.id>
2024-09-11 15:29:38 +0700

    4.6*: Helper Functions and Unit Tests, step 4

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -21,8 +23,23 @@ const favoriteBlog = (blogs) => {
   return topLikedArticle
 }
 
+const mostBlogs = (blogs) => {
+  // Group blogs by author
+  const blogsByAuthor = _.groupBy(blogs, 'author');
+
+  // Map each author to their number of blogs
+  const authorBlogCounts = _.map(blogsByAuthor, (blogs, author) => ({
+    author: author,
+    blogs: blogs.length
+  }));
+
+  // Find the author with the maximum number of blogs
+  return _.maxBy(authorBlogCounts, 'blogs');
+};
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
