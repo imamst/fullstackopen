cc52d039bdf839d07e716ccd42fbdb2143c34d27
Imam Setiawan <imam.setiawan@javan.co.id>
2024-09-16 14:10:07 +0700

    async/await and test post create blog

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -4,6 +4,7 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
 
 mongoose.set('strictQuery', false)
 
@@ -11,7 +12,11 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -13,11 +13,11 @@ test('blogs are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
-test('there are one blogs', async () => {
-  const response = await api.get('/api/blogs')
+// test('there are one blogs', async () => {
+//   const response = await api.get('/api/blogs')
 
-  assert.strictEqual(response.body.length, 1)
-})
+//   assert.strictEqual(response.body.length, 1)
+// })
 
 test('the first blog is about how to success', async () => {
   const response = await api.get('/api/blogs')
@@ -25,6 +25,50 @@ test('the first blog is about how to success', async () => {
   const contents = response.body.map(e => e.title)
 
   assert.strictEqual(contents.find(content => content.includes('to Success')).length > 0, true)
+})
+
+test('a valid blog can be added', async () => {
+  const initialData = await api.get('/api/blogs')
+
+  const newBlog = {
+    "title": "Blogging is Easy",
+    "author": "Imam Setiawan",
+    "url": "http://google.co.id",
+    "likes": 23
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+  
+  const response = await api.get('/api/blogs')
+
+  const contents = response.body.map(e => e.title)
+
+  assert.strictEqual(response.body.length, initialData.body.length + 1)
+
+  assert(contents.includes('Blogging is Easy'))
+})
+
+test('blog without title is not added', async () => {
+  const initialData = await api.get('/api/blogs')
+
+  const newBlog = {
+    "author": "Imam Setiawan",
+    "url": "http://google.co.id",
+    "likes": 23
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+  
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, initialData.body.length)
 })
 
 after(async () => {
