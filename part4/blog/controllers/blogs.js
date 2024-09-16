876dc79dbaafc5606c463a993b19ea037aee8be3
Imam Setiawan <imam.setiawan@javan.co.id>
2024-09-16 14:31:44 +0700

    eliminating try-catch by using express-async-errors lib

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -2,43 +2,31 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response, next) => {
-  const blogs = await Blog.find({}).catch(error => next(error))
-  
+  const blogs = await Blog.find({})
+
   response.json(blogs);
 })
 
 blogsRouter.post('/', async (request, response, next) => {
   const blog = new Blog(request.body)
   
-  try {
-    const savedBlog = await blog.save()
-    response.status(201).json(savedBlog)
-  } catch (error) {
-    next(error)
-  }
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 })
 
 blogsRouter.get('/:id', async (request, response, next) => {
-  try {
-    const blog = await Blog.findById(request.params.id);
-    if (blog) {
-      response.json(blog)
-    } else {
-      response.status(404).end()
-    }
-  } catch (error) {
-    next(error)
+  const blog = await Blog.findById(request.params.id);
+  if (blog) {
+    response.json(blog)
+  } else {
+    response.status(404).end()
   }
 })
 
 blogsRouter.delete('/:id', async (request, response, next) => {
-  try {
-    const id = request.params.id
-    await Blog.findByIdAndDelete(id)
-    response.status(204).end()
-  } catch (error) {
-    next(error)
-  }
+  const id = request.params.id
+  await Blog.findByIdAndDelete(id)
+  response.status(204).end()
 })
 
 module.exports = blogsRouter
