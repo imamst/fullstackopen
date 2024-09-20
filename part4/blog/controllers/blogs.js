7ec2dea3ff5f3ec1834bfffbd49f21f029bc34b5
Imam Setiawan <imam.setiawan@javan.co.id>
2024-09-20 15:47:14 +0700

    handle create blog with user

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,5 +1,6 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response, next) => {
   const blogs = await Blog.find({})
@@ -8,9 +9,21 @@ blogsRouter.get('/', async (request, response, next) => {
 })
 
 blogsRouter.post('/', async (request, response, next) => {
-  const blog = new Blog(request.body)
+  const body = request.body
+  const user = await User.findById(body.userId)
+
+  const blog = new Blog({
+    title: body.title,
+    url: body.url,
+    likes: body.likes,
+    author: body.author,
+    user: user.id
+  })
   
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog.id)
+  await user.save()
+  
   response.status(201).json(savedBlog)
 })
 
