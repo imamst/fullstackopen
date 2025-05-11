1f4493e8e01e4f719ca86d132f0bb52a69844e61
Imam Setiawan <s.imam29@gmail.com>
2025-05-11 16:56:16 +0700

    5.7 Blog List Frontend, step 7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,56 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [detailsVisible, setDetailsVisible] = useState(false)
+
+  const toggleDetails = () => {
+    setDetailsVisible(!detailsVisible)
+  }
+
+  const parentStyle = {
+    border: '1px solid #ccc',
+    padding: 10,
+  }
+
+  const titleStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    paddingBottom: 10,
+    display: 'flex',
+    gap: 10,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  }
+
+  const likesStyle = {
+    display: 'flex',
+    gap: 10,
+    alignItems: 'center',
+  }
+
+  return (
+    <div style={parentStyle}>
+      <div style={titleStyle}>
+        <span>{blog.title}</span>
+        <button
+          onClick={toggleDetails}
+          className="px-4 py-2 text-blue-500 font-medium rounded-lg shadow-md hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+        >{detailsVisible ? 'hide' : 'view'}</button>
+      </div>
+      {detailsVisible && (
+        <div>
+          <p>{blog.url}</p>
+          <div style={likesStyle}>
+            <p>likes {blog.likes}</p>
+            <button
+              className="px-4 py-2 text-purple-500 font-medium rounded-lg shadow-md hover:bg-purple-200 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2"
+            >like</button>
+          </div>
+          <p>{blog.author}</p>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
