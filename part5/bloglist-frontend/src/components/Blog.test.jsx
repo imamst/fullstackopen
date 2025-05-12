3d94adccef9884579063796ae7453e1ee0fc7dac
Imam Setiawan <s.imam29@gmail.com>
2025-05-12 16:31:51 +0700

    5.14: Blog List Tests, step 2 show url and likes when the button view details has been clicked

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,7 +1,8 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders blog title and author', () => {
+test('renders blog title and author on initial', () => {
   const blog = {
     title: 'Test Blog',
     author: 'Test Author',
@@ -18,4 +19,26 @@ test('renders blog title and author', () => {
   expect(div).toHaveTextContent('Test Author')
   expect(div).not.toHaveTextContent('https://test.com')
   expect(div).not.toHaveTextContent('likes 10')
+})
+
+test('renders blog url and likes when clicked button view', async () => {
+  const user = userEvent.setup()
+  const blog = {
+    title: 'Test Blog',
+    author: 'Test Author',
+    url: 'https://test.com',
+    likes: 10,
+  }
+
+  const { container } = render(<Blog blog={blog} setIsUpdated={() => {}} />)
+
+  const viewButton = screen.getByText('view')
+
+  screen.debug(viewButton)
+
+  await user.click(viewButton)
+
+  const div = container.querySelector('.blog-container')
+  expect(div).toHaveTextContent('https://test.com')
+  expect(div).toHaveTextContent('likes 10')
 })
