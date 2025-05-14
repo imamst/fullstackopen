463a688e754b9e445aaed9a91460ff4e6ba722a5
Imam Setiawan <s.imam29@gmail.com>
2025-05-14 20:17:08 +0700

    5.18: Blog List End To End Testing, step 2 5.18: Blog List End To End Testing, step 2

--- playwright/tests/blog_app.spec.js
+++ playwright/tests/blog_app.spec.js
@@ -1,24 +1,54 @@
 import { test, expect, describe, beforeEach } from '@playwright/test';
 
 describe('Blog app', () => {
-    beforeEach(async ({ page }) => {
-        await page.goto('http://localhost:5173');
+    beforeEach(async ({ page, request }) => {
+        await request.post('http://localhost:3001/api/testing/reset')
+        await request.post('http://localhost:3001/api/users', {
+            data: {
+                name: 'Imam Setiawan',
+                username: 'imamst',
+                password: 'salainen'
+            }
+        })
+
+        await page.goto('http://localhost:5173')
     });
 
-    test('login page has expected elements', async ({ page }) => {
-    const blogTextLocator = page.getByText('Blogs');
-    await expect(blogTextLocator).toBeVisible();
+    test('Login form is shown', async ({ page }) => {
+        const blogTextLocator = page.getByText('Blogs');
+        await expect(blogTextLocator).toBeVisible();
 
-    const loginTextLocator = page.getByText('Log in to application');
-    await expect(loginTextLocator).toBeVisible();
+        const loginTextLocator = page.getByText('Log in to application');
+        await expect(loginTextLocator).toBeVisible();
 
-    const usernameInputLocator = page.getByLabel('username');
-    await expect(usernameInputLocator).toBeVisible();
+        const usernameInputLocator = page.getByLabel('username');
+        await expect(usernameInputLocator).toBeVisible();
 
-    const passwordInputLocator = page.getByLabel('password');
-    await expect(passwordInputLocator).toBeVisible();
+        const passwordInputLocator = page.getByLabel('password');
+        await expect(passwordInputLocator).toBeVisible();
 
-    const loginButtonLocator = page.getByRole('button', { name: 'Login' });
-    await expect(loginButtonLocator).toBeVisible();
+        const loginButtonLocator = page.getByRole('button', { name: 'Login' });
+        await expect(loginButtonLocator).toBeVisible();
     });
+
+    describe('Login', () => {
+        test('succeeds with correct credentials', async ({ page }) => {
+            await page.getByLabel('username').fill('imamst')
+            await page.getByLabel('password').fill('salainen')
+            await page.getByRole('button', { name: 'Login' }).click()
+
+            await expect(page.getByText('Imam Setiawan logged-in')).toBeVisible()
+            await expect(page.getByText('Blogs')).toBeVisible()
+            await expect(page.getByText('Logout', { exact: true })).toBeVisible()
+            await expect(page.getByText('create new blog', { exact: true })).toBeVisible()
+        })
+
+        test('fails with wrong credentials', async ({ page }) => {
+            await page.getByLabel('username').fill('imamst')
+            await page.getByLabel('password').fill('wrong')
+            await page.getByRole('button', { name: 'Login' }).click()
+
+            await expect(page.getByText('Wrong credentials')).toBeVisible()
+        })
+    })
 });
