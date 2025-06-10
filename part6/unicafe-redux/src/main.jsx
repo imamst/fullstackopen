9fd80c50787bbfc7f145566c8b124e0ad24bbe9e
Imam Setiawan <s.imam29@gmail.com>
2025-06-11 06:56:56 +0700

    6.1: Unicafe Revisited, step 1 https://fullstackopen.com/en/part6/flux_architecture_and_redux#exercises-6-1-6-2

--- part6/unicafe-redux/src/main.jsx
+++ part6/unicafe-redux/src/main.jsx
@@ -7,21 +7,21 @@ import reducer from './reducer'
 const store = createStore(reducer)
 
 const App = () => {
-  const good = () => {
+  const vote = (type) => {
     store.dispatch({
-      type: 'GOOD'
+      type
     })
   }
 
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>ok</button> 
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={() => vote('GOOD')}>good</button> 
+      <button onClick={() => vote('OK')}>ok</button> 
+      <button onClick={() => vote('BAD')}>bad</button>
+      <button onClick={() => vote('ZERO')}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
