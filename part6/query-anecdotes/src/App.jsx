d67d51a3cecb30a6860f5f00d72fe7bc8eb29e57
Imam Setiawan <s.imam29@gmail.com>
2025-07-07 20:37:57 +0700

    Exercise 6.20 - 6.21 Implement retrieving anecdotes from the server using React Query and handle if server error

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,5 +1,7 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { useQuery } from '@tanstack/react-query'
+import { getAnecdotes } from './requests'
 
 const App = () => {
 
@@ -7,13 +9,19 @@ const App = () => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const anecdotes = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    retry: false
+  })
+
+  if (anecdotes.isLoading) {
+    return <div>loading...</div>
+  }
+
+  if (anecdotes.isError) {
+    return <div>anecdotes service not available due to problems in server</div>
+  }
 
   return (
     <div>
@@ -22,7 +30,7 @@ const App = () => {
       <Notification />
       <AnecdoteForm />
     
-      {anecdotes.map(anecdote =>
+      {anecdotes.data?.map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
